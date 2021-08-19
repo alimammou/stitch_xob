ec0536d49ee2e2e8562181e16a46fe03d37cb2f7
alimammou <ali.moumou.a.m@gmail.com>
2021-08-19 14:33:34 +0300

    1

--- database/migrations/2021_08_19_112400_create_pointspurchases_table.php
+++ database/migrations/2021_08_19_112400_create_pointspurchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Storepurchases extends Migration
+class CreatePointspurchasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class Storepurchases extends Migration
      */
     public function up()
     {
-        Schema::create('storepurchases', function (Blueprint $table) {
+        Schema::create('pointspurchases', function (Blueprint $table) {
             $table->id();
-            $table->string('storeitem_id');
-            $table->string('storecode_id');
             $table->string('user_id');
-            $table->string('purchasedfor');
+            $table->string('invoiceid');
+            $table->string('invoiceconfirmed');
+            $table->string('amount_in_usd');
+            $table->string('status');
+            $table->string('rate');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class Storepurchases extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('pointspurchases');
     }
 }
